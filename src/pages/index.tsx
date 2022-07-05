9076ce6c8c4ec6ca755b309eb617648703032f30
HyeonJun Jeon <teddy3582@gmail.com>
2022-07-06 06:27:32 +0900

    Page Link / Slug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,6 +106,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
